77aa8f967d9ac3c7d874c3d6fc94ce3024cb39cb
pietrolesci <pietrolesci@outlook.com>
2023-02-11 16:42:50 +0000

    enhance train.py logging

--- scripts/train.py
+++ scripts/train.py
@@ -3,14 +3,13 @@ import pickle
 from pathlib import Path
 
 import hydra
-import srsly
 from datasets import load_from_disk
 from hydra.utils import get_original_cwd
 from lightning.fabric import seed_everything
 from omegaconf import DictConfig, OmegaConf
 
 from src.data.datamodule import ClassificationDataModule
-from src.estimators.classification import EstimatorForSequenceClassification
+from src.transformers import EstimatorForSequenceClassification
 from src.logging import set_ignore_warnings
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
@@ -23,6 +22,13 @@ sep_line = f"{'=' * 70}"
 @hydra.main(version_base=None, config_path="../conf", config_name="conf")
 def main(cfg: DictConfig):
 
+    # set paths, load metadata, and tidy config
+    data_path = Path(get_original_cwd()) / "data" / "prepared" / cfg.dataset_name
+    
+    metadata = OmegaConf.load(data_path / "metadata.yaml")
+    if cfg.model.name_or_path is None:
+        cfg.model.name_or_path = metadata.name_or_path
+    
     log.info(f"\n{OmegaConf.to_yaml(cfg, resolve=True)}\n{sep_line}")
     if cfg.dry_run:
         log.critical("\n\n\t !!! DEBUGGING !!! \n\n")
@@ -30,14 +36,10 @@ def main(cfg: DictConfig):
     # seed everything
     seed_everything(cfg.seed)
 
-    # load data and metadata
-    data_path = Path(get_original_cwd()) / "data" / "prepared" / cfg.dataset_name
-    metadata = OmegaConf.load(data_path / "metadata.yaml")
+    # load data    
     dataset_dict = load_from_disk(data_path, keep_in_memory=True)
 
     # load tokenizer
-    if cfg.model.name_or_path is None:
-        cfg.model.name_or_path = metadata.name_or_path
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.name_or_path)
 
     # define datamodule
@@ -58,13 +60,13 @@ def main(cfg: DictConfig):
 
     # fit
     train_outputs = estimator.fit(
-        train_loader=datamodule.train_dataloader(),
-        validation_loader=datamodule.val_dataloader(),
+        train_loader=datamodule.train_loader(),
+        validation_loader=datamodule.validation_loader(),
         **cfg.fit,
     )
 
     # test
-    test_outputs = estimator.test(datamodule.test_dataloader(), **cfg.test)
+    test_outputs = estimator.test(datamodule.test_loader(), **cfg.test)
 
     # save experiment output
     with open("./train_outputs", "wb") as fl:
