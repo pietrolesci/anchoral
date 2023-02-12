447cfa4e546122a485108cbb6e8068d3dee25c6c
pietrolesci <pietrolesci@outlook.com>
2023-02-12 19:48:06 +0000

    add save output callback

--- src/huggingface/callbacks.py
+++ src/huggingface/callbacks.py
@@ -13,46 +13,54 @@ from src.utilities import move_to_cpu
 
 
 class SaveOutputs(Callback):
-    def __init__(self, dirpath: Union[str, Path]) -> None:
+    def __init__(self, dirpath: Union[str, Path], instance_level: bool, batch_level: bool, epoch_level: bool) -> None:
         super().__init__()
         self.dirpath = Path(dirpath)
+        self.instance_level = instance_level
+        self.batch_level = batch_level
+        self.epoch_level = epoch_level
 
     def on_epoch_end(
-        self, model: _FabricModule, output: EPOCH_OUTPUT, metrics: METRIC, stage: RunningStage, **kwargs
+        self,
+        model: _FabricModule,
+        output: EPOCH_OUTPUT,
+        metrics: METRIC,
+        stage: RunningStage,
+        **kwargs,
     ) -> None:
+        # output directory setup
         epoch_idx = kwargs.get("epoch_idx", None)
         suffix = f"_epoch_{epoch_idx}" if epoch_idx is not None else ""
+        path = self.dirpath / f"{stage}"
+        path.mkdir(exist_ok=True, parents=True)
 
         # list of dicts to dict of lists
         data = {k: [dic[k] for dic in output] for k in output[0]}
 
         # instance-level output
-        logits = np.concatenate(data.pop(OutputKeys.LOGITS))
-        idx = np.concatenate(data.pop(SpecialKeys.ID))
-        instance_level_output = pd.DataFrame(logits, columns=[f"logit_{i}" for i in range(logits.shape[1])]).assign(
-            **{SpecialKeys.ID: idx}
-        )
+        if self.instance_level:
+            logits = np.concatenate(data.pop(OutputKeys.LOGITS))
+            idx = np.concatenate(data.pop(SpecialKeys.ID))
+            instance_level_output = pd.DataFrame(logits, columns=[f"logit_{i}" for i in range(logits.shape[1])]).assign(
+                **{SpecialKeys.ID: idx}
+            )
+            instance_level_output.to_parquet(path / f"instance_level{suffix}.parquet", index=False)
 
         # batch-level output
-        loss = data.pop(OutputKeys.LOSS)
-        batch_metrics = data.pop(OutputKeys.METRICS)
-        batch_metrics = {k: [dic[k] for dic in batch_metrics] for k in batch_metrics[0]}
-        batch_level_output = pd.DataFrame(batch_metrics).assign(loss=loss)
+        if self.batch_level:
+            batch_metrics = data.pop(OutputKeys.METRICS)
+            batch_metrics = {k: [dic[k] for dic in batch_metrics] for k in batch_metrics[0]}
+            batch_level_output = pd.DataFrame(batch_metrics).assign(loss=data[OutputKeys.LOSS])
+            batch_level_output.to_parquet(path / f"batch_level{suffix}.parquet", index=False)
 
         # epoch-level output
-        total_metrics = move_to_cpu(metrics.compute())
-        epoch_level_output = {
-            "metrics": total_metrics,
-            "loss": round(np.mean(loss), 6),
-        }
-
-        # save
-        path = self.dirpath / f"{stage}"
-        path.mkdir(exist_ok=True, parents=True)
-
-        instance_level_output.to_parquet(path / f"instance_level{suffix}.parquet", index=False)
-        batch_level_output.to_parquet(path / f"batch_level{suffix}.parquet", index=False)
-        srsly.write_json(path / f"epoch_level{suffix}.json", epoch_level_output)
+        if self.epoch_level:
+            total_metrics = move_to_cpu(metrics.compute())
+            epoch_level_output = {
+                OutputKeys.METRICS: total_metrics,
+                OutputKeys.LOSS: round(np.mean(data[OutputKeys.LOSS]), 6),
+            }
+            srsly.write_json(path / f"epoch_level{suffix}.json", epoch_level_output)
 
     def on_train_epoch_end(self, model: _FabricModule, output: EPOCH_OUTPUT, metrics: METRIC, **kwargs) -> None:
         return self.on_epoch_end(model, output, metrics, RunningStage.TRAIN, **kwargs)
