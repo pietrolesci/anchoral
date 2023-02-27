ffef1489d3cea415bfc069681dc870e81094d2bc
pietrolesci <pietrolesci@outlook.com>
2023-02-27 22:46:19 +0000

    fix validation batches

--- src/energizer/progress_trackers.py
+++ src/energizer/progress_trackers.py
@@ -283,10 +283,12 @@ class ProgressTracker:
             validation_loader, kwargs.get("limit_validation_batches", None)
         )
         validation_interval = kwargs.get("validation_interval", True)
-        if max_validation_batches is not None and validation_interval is not None:
+        if max_validation_batches is not None and validation_interval is not None and max_train_batches > validation_interval:
             validation_interval = np.linspace(
                 max_train_batches / validation_interval, max_train_batches, validation_interval, dtype=int
             ).tolist()[:-1]
+        else:
+            validation_interval = None
 
         return {
             "num_epochs": num_epochs,
