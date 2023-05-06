2418f8814596922f13b06ebaef5137fbdf4ae54d
Pietro Lesci <61748653+pietrolesci@users.noreply.github.com>
2023-05-06 22:44:00 +0100

    Dev3 (#5)
    * update
    
    * reproducibility
    
    * reproducibility
    
    * finally it works
    
    * finally it works
    
    * finally it works
    
    * finally it works
    
    * finally it works
    
    * should validate fix
    
    * repro
    
    * should validate fix
    
    * model selection callbacks
    
    * uncertainty (#1)
    
    * add grad norm callback
    
    * experiment config
    
    * better budget management
    
    * add joblib laucher
    
    * clean config
    
    * update pool loader configuration
    
    * fix index retrieval
    
    * add new strategies take 1
    
    * update
    
    * add proper energizer package

--- src/callbacks.py
+++ src/callbacks.py
@@ -5,17 +5,15 @@ import pandas as pd
 import srsly
 from lightning.fabric.wrappers import _FabricModule
 
-from src.energizer.active_learning.active_estimator import ActiveEstimator
-from src.energizer.active_learning.callbacks import ActiveLearningCallbackMixin
-from src.energizer.active_learning.data import ActiveDataModule
-from src.energizer.callbacks.base import Callback
-from src.energizer.enums import Interval, OutputKeys, RunningStage, SpecialKeys
-from src.energizer.estimator import Estimator
-from src.energizer.types import EPOCH_OUTPUT, METRIC, ROUND_OUTPUT
-from src.energizer.utilities import make_dict_json_serializable
+from energizer.callbacks import Callback
+from energizer.datastores import PandasDataStoreForSequenceClassification
+from energizer.enums import Interval, OutputKeys, RunningStage, SpecialKeys
+from energizer.estimators.active_estimator import ActiveEstimator
+from energizer.types import METRIC, ROUND_OUTPUT
+from energizer.utilities import make_dict_json_serializable, move_to_cpu
 
 
-class SaveOutputs(ActiveLearningCallbackMixin, Callback):
+class SaveOutputs(Callback):
     """All the logic to save artifacts and log."""
 
     def __init__(self, dirpath: Union[str, Path], instance_level: bool, batch_level: bool, epoch_level: bool) -> None:
@@ -25,50 +23,10 @@ class SaveOutputs(ActiveLearningCallbackMixin, Callback):
         self.batch_level = batch_level
         self.epoch_level = epoch_level
 
-    """
-    Method forwarding
-    """
-
-    def on_train_epoch_end(
-        self,
-        estimator: Union[Estimator, ActiveEstimator],
-        model: _FabricModule,
-        output: EPOCH_OUTPUT,
-        metrics: METRIC,
-        **kwargs,
+    def on_epoch_end(
+        self, stage: RunningStage, estimator: ActiveEstimator, model: _FabricModule, output: Dict, metrics: METRIC
     ) -> None:
-        self.on_epoch_end(estimator, output, RunningStage.TRAIN)
 
-    def on_validation_epoch_end(
-        self,
-        estimator: Union[Estimator, ActiveEstimator],
-        model: _FabricModule,
-        output: EPOCH_OUTPUT,
-        metrics: METRIC,
-        **kwargs,
-    ) -> None:
-        self.on_epoch_end(estimator, output, RunningStage.VALIDATION)
-
-    def on_test_epoch_end(
-        self,
-        estimator: Union[Estimator, ActiveEstimator],
-        model: _FabricModule,
-        output: EPOCH_OUTPUT,
-        metrics: METRIC,
-        **kwargs,
-    ) -> None:
-        self.on_epoch_end(estimator, output, RunningStage.TEST)
-
-    def on_pool_epoch_end(
-        self, estimator: ActiveEstimator, model: _FabricModule, output: EPOCH_OUTPUT, metrics: METRIC
-    ) -> None:
-        self.on_epoch_end(estimator, output, RunningStage.POOL)
-
-    """
-    Actual methods
-    """
-
-    def on_epoch_end(self, estimator: Union[Estimator, ActiveEstimator], output: Dict, stage: RunningStage) -> None:
         # output directory setup
         path = self.dirpath / f"{stage}"
         if not path.exists():
@@ -76,9 +34,7 @@ class SaveOutputs(ActiveLearningCallbackMixin, Callback):
 
         # instance-level output
         if self.instance_level:
-            data = pd.DataFrame(
-                columns=[f"logit_{i}" for i in range(estimator.model.num_labels)], data=output[OutputKeys.LOGITS]
-            )
+            data = pd.DataFrame(columns=[f"logit_{i}" for i in range(model.num_labels)], data=output[OutputKeys.LOGITS])
             data[SpecialKeys.ID] = output[SpecialKeys.ID]
             data[Interval.EPOCH] = estimator.progress_tracker.safe_global_epoch
 
@@ -102,7 +58,7 @@ class SaveOutputs(ActiveLearningCallbackMixin, Callback):
                 "stage": stage,
                 Interval.EPOCH: estimator.progress_tracker.safe_global_epoch,
                 OutputKeys.LOSS: output[OutputKeys.LOSS],
-                **output[OutputKeys.METRICS],
+                **move_to_cpu(metrics.compute()),
             }
             if hasattr(estimator.progress_tracker, "global_round"):
                 data[Interval.ROUND] = estimator.progress_tracker.global_round
@@ -112,11 +68,16 @@ class SaveOutputs(ActiveLearningCallbackMixin, Callback):
                 path / "epoch_level.jsonl", [make_dict_json_serializable(data)], append=True, append_new_line=False
             )
 
-    def on_round_end(self, estimator: ActiveEstimator, datamodule: ActiveDataModule, output: ROUND_OUTPUT) -> None:
+    def on_round_end(
+        self, estimator: ActiveEstimator, datastore: PandasDataStoreForSequenceClassification, output: ROUND_OUTPUT
+    ) -> None:
         # save partial results
-        datamodule.save_labelled_dataset(self.dirpath)
+        datastore.save_labelled_dataset(self.dirpath)
 
     def on_active_fit_end(
-        self, estimator: ActiveEstimator, datamodule: ActiveDataModule, output: List[ROUND_OUTPUT]
+        self,
+        estimator: ActiveEstimator,
+        datastore: PandasDataStoreForSequenceClassification,
+        output: List[ROUND_OUTPUT],
     ) -> None:
-        datamodule.save_labelled_dataset(self.dirpath)
+        datastore.save_labelled_dataset(self.dirpath)
--- src/estimators.py
+++ src/estimators.py
@@ -1,120 +1,58 @@
-# NOTE: type annotation is in line with what it is actually returned by
-# the overridden methods
-from typing import Callable, Dict, List, MutableMapping, Optional, Union
+from typing import Dict, List, Optional
 
 import numpy as np
-import torch
 from lightning.fabric.wrappers import _FabricModule
-from lightning.pytorch.utilities.parsing import AttributeDict
 from torchmetrics import MetricCollection
-from torchmetrics.classification import Accuracy, F1Score, Precision, Recall
-from transformers import AutoModelForSequenceClassification
+from torchmetrics.classification import AUROC, Accuracy, AveragePrecision, F1Score, Precision, Recall
 
-from src.energizer.active_learning.active_estimator import RoundOutput
-from src.energizer.active_learning.data import ActiveDataModule
-from src.energizer.active_learning.strategies import RandomStrategy, UncertaintyBasedStrategy
-from src.energizer.enums import InputKeys, OutputKeys, RunningStage, SpecialKeys
-from src.energizer.estimator import Estimator
-from src.energizer.types import ROUND_OUTPUT, Dict
-from src.energizer.utilities import ld_to_dl, move_to_cpu
-
-
-def resolve_strategy_name(s: str) -> str:
-    if "UncertaintyBasedStrategyForSequenceClassification" in s["_target_"]:
-        return s["score_fn"]
-    return "random"
+from energizer.enums import InputKeys, OutputKeys, RunningStage, SpecialKeys
+from energizer.estimators import Estimator as _Estimator
+from energizer.strategies import RandomStrategy as _RandomStrategy
+from energizer.strategies import UncertaintyBasedStrategy as _UncertaintyBasedStrategy
+from energizer.types import ROUND_OUTPUT, Dict
+from energizer.utilities import ld_to_dl, move_to_cpu
 
 
 class SequenceClassificationMixin:
+    def configure_metrics(self, *_) -> MetricCollection:
+        num_classes = self.model.num_labels  # type: ignore
+        task = "multiclass"
+        return MetricCollection(
+            {
+                "accuracy_macro": Accuracy(task, num_classes=num_classes, average="macro"),
+                "f1_macro": F1Score(task, num_classes=num_classes, average="macro"),
+                "precision_macro": Precision(task, num_classes=num_classes, average="macro"),
+                "recall_macro": Recall(task, num_classes=num_classes, average="macro"),
+                "average_precision_macro": AveragePrecision(task, num_classes=num_classes, average="macro"),
+                "auroc": AUROC(task, num_classes=num_classes, average="macro"),
+                "accuracy_micro": Accuracy(task, num_classes=num_classes, average="micro"),
+                "f1_micro": F1Score(task, num_classes=num_classes, average="micro"),
+                "precision_micro": Precision(task, num_classes=num_classes, average="micro"),
+                "recall_micro": Recall(task, num_classes=num_classes, average="micro"),
+            }
+        ).to(
+            self.device
+        )  # type: ignore
 
-    """
-    Method forwarding
-    """
-
-    def train_step(
-        self,
-        model: _FabricModule,
-        batch: Dict,
-        batch_idx: int,
-        loss_fn: Optional[Union[torch.nn.Module, Callable]],
-        metrics: MetricCollection,
-    ) -> Dict:
-        return self.step(model, batch, batch_idx, loss_fn, metrics, RunningStage.TRAIN)
-
-    def validation_step(
-        self,
-        model: _FabricModule,
-        batch: Dict,
-        batch_idx: int,
-        loss_fn: Optional[Union[torch.nn.Module, Callable]],
-        metrics: MetricCollection,
-    ) -> Dict:
-        return self.step(model, batch, batch_idx, loss_fn, metrics, RunningStage.VALIDATION)
-
-    def test_step(
-        self,
-        model: _FabricModule,
-        batch: Dict,
-        batch_idx: int,
-        loss_fn: Optional[Union[torch.nn.Module, Callable]],
-        metrics: MetricCollection,
-    ) -> Dict:
-        return self.step(model, batch, batch_idx, loss_fn, metrics, RunningStage.TEST)
-
-    def train_epoch_end(self, output: List[Dict], metrics: MetricCollection) -> Dict:
-        return self.epoch_end(output, metrics, RunningStage.TRAIN)
-
-    def validation_epoch_end(self, output: List[Dict], metrics: MetricCollection) -> Dict:
-        return self.epoch_end(output, metrics, RunningStage.VALIDATION)
-
-    def test_epoch_end(self, output: List[Dict], metrics: MetricCollection) -> Dict:
-        return self.epoch_end(output, metrics, RunningStage.TEST)
-
-    def pool_epoch_end(self, output: List[Dict], metrics: MetricCollection) -> Dict:
-        return self.epoch_end(output, metrics, RunningStage.POOL)
-
-    """
-    Actual methods
-    """
-
-    def step(
-        self,
-        model: AutoModelForSequenceClassification,
-        batch: Dict,
-        batch_idx: int,
-        loss_fn: Optional[Union[torch.nn.Module, Callable]],
-        metrics: MetricCollection,
-        stage: RunningStage,
-    ) -> Dict:
-        """This serves as train/validation/test step for transformers classifiers."""
-        unique_ids = batch.pop(InputKeys.ON_CPU)[SpecialKeys.ID]
-
-        # forward pass
+    def step(self, model: _FabricModule, batch: Dict, metrics: MetricCollection, stage: RunningStage) -> Dict:
+        ids = batch.pop(InputKeys.ON_CPU, None)
         out = model(**batch)
-
-        # compute weighted loss if provided otherwise use the
-        # unweighted loss automaticaly computed by transformers
-        loss = loss_fn(out.logits, batch[InputKeys.TARGET]) if loss_fn is not None else out.loss
-
-        # compute metrics
-        # NOTE: we do not return metrics since they can be aggregated using `metrics.compute()` later
         out_metrics = metrics(out.logits, batch[InputKeys.TARGET])
 
-        # log batch-level metrics
         if stage == RunningStage.TRAIN:
-            # NOTE: only log at the batch level training
-            logs = {OutputKeys.LOSS: loss, **out_metrics}
-            self.log_dict({f"{stage}/{k}": v for k, v in logs.items()}, step=self.progress_tracker.global_batch)
+            logs = {OutputKeys.LOSS: out.loss, **out_metrics}
+            self.log_dict({f"{stage}/{k}": v for k, v in logs.items()}, step=self.progress_tracker.global_batch)  # type: ignore
 
         return {
-            OutputKeys.LOSS: loss,
+            OutputKeys.LOSS: out.loss,
             OutputKeys.LOGITS: out.logits,
-            SpecialKeys.ID: unique_ids,
             OutputKeys.METRICS: out_metrics,
+            SpecialKeys.ID: ids,
         }
 
     def epoch_end(self, output: List[Dict], metrics: MetricCollection, stage: RunningStage) -> Dict:
         """Aggregate and log metrics after each train/validation/test/pool epoch."""
+
         data = ld_to_dl(output)
 
         # aggregate instance-level metrics
@@ -129,77 +67,47 @@ class SequenceClassificationMixin:
         aggregated_metrics = move_to_cpu(metrics.compute())  # NOTE: metrics are still on device
         aggregated_loss = round(np.mean(data[OutputKeys.LOSS]), 6)
         logs = {OutputKeys.LOSS: aggregated_loss, **aggregated_metrics}
-
         logs = {f"{stage}_end/{k}": v for k, v in logs.items()}
-        self.log_dict(logs, step=self.progress_tracker.safe_global_epoch)
+        self.log_dict(logs, step=self.progress_tracker.safe_global_epoch)  # type: ignore
 
-        if stage == RunningStage.TEST and hasattr(self.progress_tracker, "global_budget"):
+        # if active_fit log with budget on the x-axis
+        if stage == RunningStage.TEST and hasattr(self.progress_tracker, "global_budget"):  # type: ignore
             logs = {f"{k}_vs_budget": v for k, v in logs.items()}
-            self.log_dict(logs, step=self.progress_tracker.global_budget)
+            self.log_dict(logs, step=self.progress_tracker.global_budget)  # type: ignore
 
-        return {
-            OutputKeys.LOSS: aggregated_loss,
-            OutputKeys.METRICS: aggregated_metrics,
-            **out,
-        }
+        return {OutputKeys.LOSS: aggregated_loss, **out}
 
-    def round_epoch_end(self, output: RoundOutput, datamodule: ActiveDataModule) -> ROUND_OUTPUT:
+    def round_epoch_end(self, output: Dict, *args, **kwargs) -> ROUND_OUTPUT:
         """Log round-level statistics."""
         logs = {
-            "max_epochs": self.progress_tracker.epoch_tracker.max,
-            "num_train_batches": self.progress_tracker.train_tracker.max,
-            "num_validation_batches": self.progress_tracker.validation_tracker.max,
-            "global_train_steps": self.progress_tracker.step_tracker.total,
+            "max_epochs": self.progress_tracker.epoch_tracker.max,  # type: ignore
+            "num_train_batches": self.progress_tracker.train_tracker.max,  # type: ignore
+            "num_validation_batches": self.progress_tracker.validation_tracker.max,  # type: ignore
+            "global_train_steps": self.progress_tracker.step_tracker.total,  # type: ignore
         }
         logs = {f"round_stats/{k}": v for k, v in logs.items()}
-        self.log_dict(logs, step=self.progress_tracker.global_round)
+        self.log_dict(logs, step=self.progress_tracker.global_round)  # type: ignore
 
         return output
 
     def active_fit_end(self, output: List[ROUND_OUTPUT]) -> Dict:
         """Log metrics at the end of training."""
-        logs = ld_to_dl([out.test[OutputKeys.METRICS] for out in output])
+        logs = ld_to_dl([out[RunningStage.TEST][OutputKeys.METRICS] for out in output])
         return {
             **{f"hparams/test_{k}": v[-1].item() for k, v in logs.items()},
             **{f"hparams/test_{k}_auc": np.trapz(v) for k, v in logs.items()},
         }
 
-    """
-    Changes
-    """
 
-    @property
-    def hparams(self) -> Union[AttributeDict, MutableMapping]:
-        hparams = super().hparams
-        hparams["name_or_path"] = self.model.name_or_path
-        return hparams
-
-    def configure_metrics(self, stage: Optional[RunningStage] = None) -> Optional[MetricCollection]:
-        if stage == RunningStage.POOL:
-            return
-        num_classes = self.model.num_labels
-        task = "multiclass"  # if num_classes > 2 else "binary"
-
-        # NOTE: you are in charge of moving it to the correct device
-        return MetricCollection(
-            {
-                "accuracy": Accuracy(task, num_classes=num_classes),
-                "f1_macro": F1Score(task, num_classes=num_classes, average="macro"),
-                "precision_macro": Precision(task, num_classes=num_classes, average="macro"),
-                "recall_macro": Recall(task, num_classes=num_classes, average="macro"),
-                "f1_micro": F1Score(task, num_classes=num_classes, average="micro"),
-                "precision_micro": Precision(task, num_classes=num_classes, average="micro"),
-                "recall_micro": Recall(task, num_classes=num_classes, average="micro"),
-                # "brier_score": MeanSquaredError(),
-            }
-        ).to(self.device)
-
-
-class EstimatorForSequenceClassification(SequenceClassificationMixin, Estimator):
+class Estimator(SequenceClassificationMixin, _Estimator):
     ...
 
 
-class UncertaintyBasedStrategyForSequenceClassification(SequenceClassificationMixin, UncertaintyBasedStrategy):
+class RandomStrategy(SequenceClassificationMixin, _RandomStrategy):
+    ...
+
+
+class UncertaintyBasedStrategy(SequenceClassificationMixin, _UncertaintyBasedStrategy):
     def pool_step(
         self,
         model: _FabricModule,
@@ -208,12 +116,7 @@ class UncertaintyBasedStrategyForSequenceClassification(SequenceClassificationMi
         metrics: Optional[MetricCollection] = None,
     ) -> Dict:
         _ = batch.pop(InputKeys.ON_CPU)  # this is already handled in the `evaluation_step`
-
         logits = model(**batch).logits
         scores = self.score_fn(logits)
 
         return {OutputKeys.SCORES: scores, OutputKeys.LOGITS: logits}
-
-
-class RandomStrategyForSequenceClassification(SequenceClassificationMixin, RandomStrategy):
-    ...
