f763aa42fc9981670a9d7d1fa58ee3dc764a69f0
pietrolesci <pietrolesci@outlook.com>
2023-05-29 00:05:43 +0100

    data processing

--- bin/prepare_data.sh
+++ bin/prepare_data.sh
@@ -11,20 +11,31 @@ MODEL=bert-tiny
 
 # echo Preparing Agnews
 # poetry run python ./scripts/prepare_data.py \
-#     --data_dir=$data_dir \
+#     --data_dir $data_dir \
 #     --model $MODEL \
+#     --downsample_prop 0.01 \
 #     --dataset agnews
 
 
 # echo Preparing Eurlex-57k
 # poetry run python ./scripts/prepare_data.py \
-#     --data_dir=$data_dir \
+#     --data_dir $data_dir \
 #     --model $MODEL \
 #     --dataset eurlex-57k
 
 
-echo Preparing Pubmed-200k-rct
+# echo Preparing Pubmed-200k-rct
+# poetry run python ./scripts/prepare_data.py \
+#     --data_dir $data_dir \
+#     --model $MODEL \
+#     --dataset pubmed-200k-rct
+
+
+echo Preparing AmazonCat-13k
 poetry run python ./scripts/prepare_data.py \
-    --data_dir=$data_dir \
+    --data_dir $data_dir \
     --model $MODEL \
-    --dataset pubmed-200k-rct
+    --downsample_test_size 30000 \
+    --dataset amazoncat-13k
+
+
