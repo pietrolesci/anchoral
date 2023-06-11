8946289bb1160baabc2e5127c635f9adddb8ac7d
pietrolesci <pietrolesci@outlook.com>
2023-06-11 02:58:38 +0100

    update conf and commands

--- bin/train.sh
+++ bin/train.sh
@@ -1,4 +1,33 @@
 
+poetry run python ./scripts/active_train.py -m \
+    +experiment=basic \
+    enable_progress_bar=false \
+    experiment_group=seeds \
+    dataset=pubmed \
+    data.eval_batch_size=256 \
+    active_fit.query_size=25 \
+    strategy=anchorswithperclasssampling \
+    strategy.num_anchors=50 \
+    strategy.anchor_strategy=kmeans \
+    seed=42,0,1994 \
+    model.seed=42,0,1994 \
+    +launcher=joblib
+
+poetry run python ./scripts/active_train.py -m \
+    +experiment=basic \
+    experiment_group=seeds \
+    dataset=pubmed \
+    data.eval_batch_size=256 \
+    active_fit.query_size=25 \
+    strategy=random \
+    seed=0,1994,2023,42,1234 \
+    +launcher=joblib
+
+
+
+
+
+
 poetry run python ./scripts/active_train.py +experiment=basic strategy=randomsubset dataset=wiki_toxic experiment_group=baselines strategy.subset_size=5000
 
 
