612ac66d80611c399e0eca68146b680cf5af9812
pietrolesci <pietrolesci@outlook.com>
2023-08-24 11:29:50 +0100

    update

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -41,6 +41,21 @@ poetry run python ./scripts/active_train.py -m \
     hydra.launcher.n_jobs=3 \
     dataset=amazon_agri \
     strategy=anchoral2 \
-    strategy.anchor_strategy_minority=kmeans_sil,uncertainty \
-    strategy.anchor_strategy_majority=kmeans_sil,uncertainty \
+    strategy.anchor_strategy_minority=kmeans_sil,uncertainty,random \
+    strategy.anchor_strategy_majority=random \
+    strategy.subpool_size=25,10000;
+
+poetry run python ./scripts/active_train.py -m \
+    +experiment=basic \
+    experiment_group=ablations \
+    active_fit.max_rounds=36 \
+    data.seed=42,0 \
+    model.seed=42,0 \
+    active_data.seed=42,0 \
+    +launcher=joblib \
+    hydra.launcher.n_jobs=3 \
+    dataset=amazon_agri \
+    strategy=anchoral2 \
+    strategy.anchor_strategy_minority=random \
+    strategy.anchor_strategy_majority=kmeans_sil,uncertainty,random \
     strategy.subpool_size=25,10000;
